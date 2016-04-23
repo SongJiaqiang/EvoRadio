626ced408fe1e0bec67e539ec1721001361106df
Jiaqiang <sjq2150@gmail.com>
2016-04-23 12:09:55 +0800

    6a98369提交遗漏

--- EvoRadio/Utils/CoreDB.swift
+++ EvoRadio/Utils/CoreDB.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+let ALLCHANNELS = "all_channels"
+let ALLNOWCHANNELS = "all_now_channels"
+
+
+
+class CoreDB {
+    
+    let leveldb: WLevelDb
+ 
+    init() {
+        leveldb = WLevelDb.sharedDb()
+    }
+    
+    func dbTest() {
+        leveldb.setObject("Hello LevelDb", forKey: "hi")
+    }
+    
+    class func clearAll() {
+        WLevelDb.sharedDb().removeAllObjects()
+    }
+    
+    class func saveAllChannels(responseData: [[String : AnyObject]]) {
+        WLevelDb.sharedDb().setObject(responseData, forKey: ALLCHANNELS)
+    }
+    
+    class func getAllChannels() -> [[String : AnyObject]]?{
+        let responseData = WLevelDb.sharedDb().objectForKey(ALLCHANNELS)
+        if let _ = responseData {
+            return responseData as? [[String : AnyObject]]
+        }
+        return nil
+    }
+    class func saveAllNowChannels(responseData: [[String : AnyObject]]) {
+        WLevelDb.sharedDb().setObject(responseData, forKey: ALLNOWCHANNELS)
+    }
+    
+    class func getAllNowChannels() -> [[String : AnyObject]]?{
+        let responseData = WLevelDb.sharedDb().objectForKey(ALLNOWCHANNELS)
+        if let _ = responseData {
+            return responseData as? [[String : AnyObject]]
+        }
+        return nil
+    }
+    
+}
--- EvoRadio/Utils/Radio.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Radio.swift
-//  EvoRadio
-//
-//  Created by Whisper-JQ on 16/4/22.
-//  Copyright © 2016年 JQTech. All rights reserved.
-//
-
-import Foundation
