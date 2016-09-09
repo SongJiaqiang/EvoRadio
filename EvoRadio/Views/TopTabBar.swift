816a86a4b0b3512fb23ba4f9bb6fb2eacaa6eacc
Jarvis <sjq2150@gmail.com>
2016-09-09 14:40:22 +0800

    顶部标签栏跟随滚动

--- EvoRadio/Views/TopTabBar.swift
+++ EvoRadio/Views/TopTabBar.swift
@@ -7,62 +7,98 @@
 //
 
 import UIKit
+import SnapKit
 
 class TopTabBar: UIView {
 
+    
+    
     var titles: [String]!
+    var contentView: UIView!
+    var label0: UILabel!
+    var label1: UILabel!
+    var label2: UILabel!
+    var topLine: UIView!
+    
+    var topLineWidth: CGFloat = 40
+    var topLineHeight: CGFloat = 2
+    
+    var contentViewLeftConstaint: Constraint!
    
     convenience init(titles: [String]) {
         self.init()
         
         self.titles = titles
+        self.backgroundColor = UIColor.blackColor()
         
         prepareUI()
     }
-
     
     func prepareUI() {
         
+        contentView = UIView()
+        addSubview(contentView)
+        contentView.snp_makeConstraints { (make) in
+            make.height.equalTo(20)
+            make.width.equalTo(Device.width())
+            contentViewLeftConstaint = make.left.equalTo(snp_left).offset(0).constraint
+//            make.right.equalTo(snp_right)
+            make.bottom.equalTo(snp_bottom)
+        }
         
-        let label0 = UILabel()
-        addSubview(label0)
-        label0.textColor = UIColor.whiteColor()
+        label0 = UILabel()
+        contentView.addSubview(label0)
         label0.font = UIFont.systemFontOfSize(10)
+        label0.textColor = UIColor.whiteColor()
+        label0.textAlignment = .Center
         label0.text = titles[0]
         label0.snp_makeConstraints { (make) in
-            make.left.equalTo(snp_left).offset(2)
-            make.bottom.equalTo(snp_bottom).offset(-2)
+            make.left.equalTo(contentView.snp_left).offset(2)
+            make.centerY.equalTo(contentView.snp_centerY)
         }
         
-        let label1 = UILabel()
-        addSubview(label1)
-        label1.textColor = UIColor.whiteColor()
+        label1 = UILabel()
+        contentView.addSubview(label1)
         label1.font = UIFont.systemFontOfSize(10)
+        label1.textColor = UIColor.whiteColor()
+        label1.textAlignment = .Center
         label1.text = titles[1]
         label1.snp_makeConstraints { (make) in
-            make.centerX.equalTo(snp_centerX)
-            make.bottom.equalTo(snp_bottom).offset(-2)
+            make.center.equalTo(contentView.snp_center)
         }
         
-        let label2 = UILabel()
-        addSubview(label2)
-        label2.textColor = UIColor.whiteColor()
+        label2 = UILabel()
+        contentView.addSubview(label2)
         label2.font = UIFont.systemFontOfSize(10)
+        label2.textColor = UIColor.whiteColor()
+        label2.textAlignment = .Center
         label2.text = titles[2]
         label2.snp_makeConstraints { (make) in
-            make.right.equalTo(snp_right).offset(-2)
-            make.bottom.equalTo(snp_bottom).offset(-2)
+            make.right.equalTo(contentView.snp_right).offset(-2)
+            make.centerY.equalTo(contentView.snp_centerY)
         }
         
-        let topLine = UIView()
+        topLine = UIView()
         addSubview(topLine)
         topLine.backgroundColor = UIColor.redColor()
         topLine.snp_makeConstraints { (make) in
-            make.width.equalTo(30)
-            make.height.equalTo(2)
+            make.size.equalTo(CGSizeMake(30, 2))
             make.centerX.equalTo(snp_centerX)
             make.top.equalTo(snp_top)
-//            make.bottom.equalTo(label1.snp_top)
         }
+        
+    }
+    
+    func updateTopLineFrame() {
+        topLineHeight = frame.size.height-20+2
+        topLineWidth = 40*2 / topLineHeight
+        if topLineWidth < 2 {
+            topLineWidth = 2
+        }
+        topLine.frame = CGRectMake((frame.size.width-40)/2, 0, topLineWidth, topLineHeight)
+    }
+    
+    func updateContentViewConstraint(offsetX: CGFloat) {
+        contentViewLeftConstaint.updateOffset(offsetX)
     }
 }
