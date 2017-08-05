816463d43dc0afe0e344ec58c03941d00debcdc4
Jarvis <sjq2150@gmail.com>
2017-08-05 16:42:48 +0800

    更新objc桥接文件
    原来使用的pch文件在xcode8.3.3中无法通过编译，更换为h文件

---
+++ EvoRadio/Resources/EvoRadio-Bridging-Header.h
@@ -0,0 +1,23 @@
+//
+//  EvoRadio-Bridging-Header.h
+//  EvoRadio
+//
+//  Created by Jarvis on 05/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#ifndef EvoRadio_Bridging_Header_h
+#define EvoRadio_Bridging_Header_h
+
+#import "WLevelDb.h"
+#import <pop/POP.h>
+
+#import "WXApi.h"
+
+#import <MBProgressHUD/MBProgressHUD.h>
+#import <StreamingKit/STKAudioPlayer.h>
+#import <StreamingKit/STKAutoRecoveringHTTPDataSource.h>
+#import <iCarousel/iCarousel.h>
+#import <ReflectionView/ReflectionView.h>
+
+#endif /* EvoRadio_Bridging_Header_h */
